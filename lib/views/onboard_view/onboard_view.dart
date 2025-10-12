60426472041c9817d20042b5a6bc55488c3762b3
Kaan <kaefean1@gmail.com>
2025-10-12 16:19:04 +0300

    app icon svg and png add, onboard view started

---
+++ lib/views/onboard_view/onboard_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnboardView extends StatefulWidget {
+  const OnboardView({super.key});
+
+  @override
+  State<OnboardView> createState() => _OnboardViewState();
+}
+
+class _OnboardViewState extends State<OnboardView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
